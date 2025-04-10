16a321612db2e666e9b699abc0c14b4d718b8aa4
Fazekas Bálint <fazekasbalint003@gmail.com>
2025-04-10 10:51:57 +0200

    Project creation

---
+++ src/test/java/com/example/progkorny/ProgkornyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progkorny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgkornyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
